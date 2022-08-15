52e9be56bdcb4033d49cd173c03c7bec2aeda662
yalem123 <yalembrhankelayneh@gmail.com>
2022-08-15 08:06:04 -0700

    edit error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,7 +5,7 @@
  * @array: pointer to array
  * @size: size of the array
  */
-void bubble_sort(int *array, size_t size)void bubble_sort(int *array, size_t size)
+void bubble_sort(int *array, size_t size)
 {
 size_t i, j, n;
 for (i = 0; i < size; i++)
